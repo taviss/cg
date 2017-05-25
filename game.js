e69f14046b8b2c54f1cc72d3f978c544bf08ffea
octavian.salcianu <octavian.salcianu@softvision.ro>
2017-05-25 17:57:13 +0300

    Progress, yey

--- game.js
+++ game.js
@@ -14,8 +14,59 @@ var map = [ // 1  2  3  4  5  6  7  8  9
 var scene, camera, controls, clock;
 var renderer;
 
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+
 const MOVE_SPEED = 100;
 const LOOK_SPEED = 0.075;
+
+var controlsEnabled = false;
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var blocker = document.getElementById( 'blocker' );
+var instructions = document.getElementById( 'instructions' );
+
+var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in'webkitPointerLockElement' in document;
+
+if ( havePointerLock ) {
+	var element = document.body;
+	var pointerlockchange = function ( event ) {
+		if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+			controlsEnabled = true;
+			controls.enabled = true;
+			blocker.style.display = 'none';
+		} else {
+			controls.enabled = false;
+			blocker.style.display = '-webkit-box';
+			blocker.style.display = '-moz-box';
+			blocker.style.display = 'box';
+			instructions.style.display = '';
+		}	
+	};
+		
+	var pointerlockerror = function ( event ) {
+		instructions.style.display = '';
+	};
+	
+	// Hook pointer lock state change events
+	document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+	document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+	document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+	document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+	document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+	document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+	
+	instructions.addEventListener( 'click', function ( event ) {
+		instructions.style.display = 'none';
+		// Ask the browser to lock the pointer
+		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+		element.requestPointerLock();
+	}, false );
+}
 
 function init() {
 	scene = new THREE.Scene();
@@ -23,6 +74,7 @@ function init() {
 	
 	camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 10000);
 	scene.add(camera);
+	camera.position.y += 25
 	
 	clock = new THREE.Clock(true);
 	
@@ -33,21 +85,22 @@ function init() {
 	controls.lookVertical = false; // Temporary solution; play on flat surfaces only
 	controls.noFly = true;*/
 	
-	var dummy = new THREE.Object3D();
-	controls = new THREE.FlyControls( dummy );
-	controls.movementSpeed = 100;
-    controls.domElement = document.body;
-    controls.rollSpeed = 0.5;
-    controls.autoForward = false;
-    controls.dragToLook = true;
+	controls = new THREE.PointerLockControls( camera );
+	scene.add(controls.getObject());
+
+	//controls.movementSpeed = 100;
+    //controls.domElement = document.body;
+    //controls.rollSpeed = 0.5;
+    //controls.autoForward = false;
+    //controls.dragToLook = true;
 	
-	scene.add(dummy);
-    dummy.add(camera);
+	//scene.add(dummy);
+    //dummy.add(camera);
 
 
 	
 	var floor = new THREE.Mesh(
-			new THREE.CubeGeometry(mapW * 250, 10, mapW * 250),
+			new THREE.CubeGeometry(mapW * 25, 10, mapW * 25),
 			new THREE.MeshLambertMaterial({color: 0xEDCBA0,/*map: THREE.ImageUtils.loadTexture('images/floor-1.jpg')*/})
 	);
 	scene.add(floor);
@@ -55,8 +108,6 @@ function init() {
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	
-	renderer.domElement.style.backgroundColor = '#D6F1FF'; // easier to see
-	document.body.appendChild(renderer.domElement);
 	
 	// Health cube
 	var healthcube = new THREE.Mesh(
@@ -73,6 +124,56 @@ function init() {
 	directionalLight2.position.set( -0.5, -1, -0.5 );
 	scene.add( directionalLight2 );
 	
+	var onKeyDown = function ( event ) {
+		switch ( event.keyCode ) {
+			case 38: // up
+			case 87: // w
+				moveForward = true;
+				break;
+			case 37: // left
+			case 65: // a
+				moveLeft = true; break;
+			case 40: // down
+			case 83: // s
+				moveBackward = true;
+				break;
+			case 39: // right
+			case 68: // d
+				moveRight = true;
+				break;
+			case 32: // space
+				if ( canJump === true ) velocity.y += 350;
+					canJump = false;
+					break;
+		}
+	};
+	
+	var onKeyUp = function ( event ) {
+		switch( event.keyCode ) {
+			case 38: // up
+			case 87: // w
+				moveForward = false;
+				break;
+			case 37: // left
+			case 65: // a
+				moveLeft = false;
+				break;
+			case 40: // down
+			case 83: // s
+				moveBackward = false;
+				break;
+			case 39: // right
+			case 68: // d
+				moveRight = false;
+				break;
+		}
+	};
+	document.addEventListener( 'keydown', onKeyDown, false );
+	document.addEventListener( 'keyup', onKeyUp, false );
+	
+	renderer.domElement.style.backgroundColor = '#D6F1FF'; // easier to see
+	document.body.appendChild(renderer.domElement);
+	
 	animate();
 }
 
@@ -86,6 +187,31 @@ function animate () {
 }
 
 function render() {
+	if ( controlsEnabled ) {
+		var time = performance.now();
+		var delta = ( time - prevTime ) / 1000;
+		velocity.x -= velocity.x * 10.0 * delta;
+		velocity.z -= velocity.z * 10.0 * delta;
+		velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+		if ( moveForward ) velocity.z -= 400.0 * delta;
+		if ( moveBackward ) velocity.z += 400.0 * delta;
+		if ( moveLeft ) velocity.x -= 400.0 * delta;
+		if ( moveRight ) velocity.x += 400.0 * delta;
+		/*if ( isOnObject === true ) {
+			velocity.y = Math.max( 0, velocity.y );
+			canJump = true;
+		}*/
+		controls.getObject().translateX( velocity.x * delta );
+		controls.getObject().translateY( velocity.y * delta );
+		controls.getObject().translateZ( velocity.z * delta );
+
+		if ( controls.getObject().position.y < 10 ) {
+			velocity.y = 0;
+			controls.getObject().position.y = 10;
+			canJump = true;
+		}
+		prevTime = time;
+	}
 	renderer.render(scene, camera);
-	controls.update(clock.getDelta());
+	//controls.update(clock.getDelta());
 }
